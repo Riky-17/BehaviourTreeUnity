997f84af85440b5e030f2b5fca3299d020865514
Riccardo <riccardospadaro58@gmail.com>
2024-04-14 16:34:54 +0200

    implemented selector node

--- BehaviourTree/Assets/Scripts/Selector.cs
+++ BehaviourTree/Assets/Scripts/Selector.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Selector : Node
 {
-   
+   public override NodeStates Evaluate()
+    {
+        if(children == null || children.Count == 0)
+            return NodeStates.Failure;
+
+        foreach (Node child in children)
+        {
+            switch(child.Evaluate())
+            {
+                case NodeStates.Failure:
+                    continue;
+                case NodeStates.Success:
+                    return NodeStates.Success;
+                case NodeStates.Running:
+                    return NodeStates.Running;
+                default:
+                    return NodeStates.Failure;
+            }
+        }
+        return NodeStates.Failure;
+    }
 }
